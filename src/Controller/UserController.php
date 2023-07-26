adcbb7ac973f388eac4760e8a0abd224c03c83bc
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-26 14:50:27 -0500

    creado form y funcional

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Users;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
@@ -27,6 +28,30 @@ class UserController extends AbstractController
 
         return $this->render("user/users.html.twig", [
             "listUsers" => $listUsers
+        ]);
+    }
+
+    /**
+     * @Route("/user/create", name="user_create")
+     */
+    public function createUser(Request $request): Response
+    {
+        $users = new Users();
+
+        $form_users = $this->createForm(\App\Form\UsersType::class, $users);
+        $form_users->handleRequest($request);
+
+        if ($form_users->isSubmitted() && $form_users->isValid()) {
+            $this->entityManager->persist($users);
+            $this->entityManager->flush();
+
+            // Redirigir a la página de éxito o hacer otra acción después de guardar el usuario
+            // Por ejemplo:
+            // return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render("user/user_create.html.twig", [
+            "form_users" => $form_users->createView(),
         ]);
     }
 }
