6bd88280369848fd70f20f9ffda793ed8640cf53
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-26 01:59:28 -0500

    Configuración de entidades

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -7,18 +7,87 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+
 class UsersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('username')
-            ->add('password')
-            ->add('address')
-            ->add('phone')
-            ->add('email')
-            ->add('authnData')
+            ->add('name', TextType::class, [
+                "label" => "Nombre",
+                "attr" => [
+                    "placeholder" => "Nombre",
+                    "autocomplete" => "off",
+                    "class" => "form-control",
+                    "required" => true
+                ]
+            ])
+            ->add('username', TextType::class, [
+                "label" => "Nombre de usuario",
+                "attr" => [
+                    "placeholder" => "Nombre de usuario",
+                    "autocomplete" => "off",
+                    "class" => "form-control",
+                    "required" => true
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                "label" => "Contraseña",
+                "attr" => [
+                    "placeholder" => "Contraseña",
+                    "autocomplete" => "off",
+                    "class" => "form-control",
+                    "required" => true
+                ]
+            ])
+            ->add('address', TextType::class, [
+                "label" => "Dirección",
+                "attr" => [
+                    "placeholder" => "Dirección",
+                    "autocomplete" => "off",
+                    "class" => "form-control",
+                    "required" => true
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                "label" => "Teléfono",
+                "attr" => [
+                    "placeholder" => "Teléfono",
+                    "autocomplete" => "off",
+                    "class" => "form-control",
+                    "required" => true
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                "label" => "Correo electrónico",
+                "attr" => [
+                    "placeholder" => "Correo electrónico",
+                    "autocomplete" => "off",
+                    "class" => "form-control",
+                    "required" => true
+                ]
+            ])
+            ->add('authData', CheckboxType::class, [
+                "label" => "Datos de autorización",
+                "attr" => [
+                    "placeholder" => "Datos de autorización",
+                    "autocomplete" => "off",
+                    "class" => "form-control",
+                    "required" => true
+                ]
+            ])
+            ->add("submit", SubmitType:class, [
+                "label" => "Guardar",
+                "attr" => [
+                    "class" => "btn btn-primary"
+                ]
+            ])
         ;
     }
 
