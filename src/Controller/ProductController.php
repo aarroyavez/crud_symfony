791b2d20d46040877a9f115a69383d0063255339
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-27 13:42:34 -0500

    probando input en products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 class ProductController extends AbstractController
 {
     private $productRepository;
-    
+
     public function __construct(ProductRepository $productRepository)
     {
         $this->productRepository = $productRepository;
@@ -20,26 +20,33 @@ class ProductController extends AbstractController
     /**
      * @Route("/products", name="get_available_products", methods={"GET"})
      */
-    public function getAvailableProducts(Request $request): JsonResponse
+    public function getAvailableProducts(Request $request, ProductRepository $productRepository): JsonResponse
     {
-        $limit = $request->query->getInt('limit', 10); // Obtener el límite máximo de productos de la consulta
-        $searchTerm = $request->query->get('search'); // Obtener el texto de búsqueda
+        // Validar el límite máximo de productos
+        $limit = intval($request->query->get('limit', 10));
+        $limit = max(1, min(100, $limit)); // Limitar el límite entre 1 y 100
 
-        $products = $this->productRepository->findAvailableProducts($limit, $searchTerm);
+        // Validar el texto de búsqueda
+        $searchTerm = trim($request->query->get('search'));
 
-        $responseData = [];
-        foreach ($products as $product) {
-            $responseData[] = [
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'available_quantity' => $product->getQuantity(),
-                'unit_price' => $product->getPrice(),
-            ];
+        // Obtener la lista de productos que cumplan con los parámetros
+        try {
+            $products = $productRepository->findAvailableProducts($limit, $searchTerm);
+
+            $responseData = [];
+            foreach ($products as $product) {
+                $responseData[] = [
+                    'id' => $product->getId(),
+                    'name' => $product->getName(),
+                    'available_quantity' => $product->getQuantity(),
+                    'unit_price' => $product->getPrice(),
+                ];
+            }
+
+            return new JsonResponse($responseData, 200);
+        } catch (\Exception $e) {
+            // En caso de excepción, enviar respuesta de error 400 con el mensaje de error
+            return new JsonResponse(['error' => 'Respuesta 400, error en la consulta: ' . $e->getMessage()], 400);
         }
-
-        return new JsonResponse($responseData, 200);
     }
 }
-
-
-
