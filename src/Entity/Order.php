213247611cfe767b0c3a5a8f46d67044e73a2948
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-27 21:34:12 -0500

    formulario para comprar pero sin productos aún

---
+++ src/Entity/Order.php
@@ -0,0 +1,80 @@
+<?php
+
+// src/Entity/Order.php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="orders")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Users")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $shippingAddress;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $total;
+
+    // Getters and setters for the properties
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?Users
+    {
+        return $this->user;
+    }
+
+    public function setUser(?Users $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getShippingAddress(): ?string
+    {
+        return $this->shippingAddress;
+    }
+
+    public function setShippingAddress(?string $shippingAddress): self
+    {
+        $this->shippingAddress = $shippingAddress;
+        return $this;
+    }
+
+    public function getTotal(): ?string
+    {
+        return $this->total;
+    }
+
+    public function setTotal(?string $total): self
+    {
+        $this->total = $total;
+        return $this;
+    }
+
+    // ... (getters and setters for other properties)
+
+}
---
+++ src/Form/ProductOrderType.php
@@ -0,0 +1,44 @@
+<?php
+// src/Form/ProductOrderType.php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ProductOrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('user', EntityType::class, [
+                'class' => 'App\Entity\Users',
+                'choice_label' => 'name',
+                'label' => 'Usuario del pedido',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('shippingAddress', TextType::class, [
+                'label' => 'Dirección de envío',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('total', NumberType::class, [
+                'label' => 'Total del pedido',
+                'attr' => ['class' => 'form-control'],
+            ]);
+            
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
